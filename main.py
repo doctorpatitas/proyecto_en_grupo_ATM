529dad4f24ec580b00b28894804b33cd3122f20b
doctorpatitas <diegogonzal272111@gmail.com>
2026-03-04 20:10:59 -0500

    Segundo menu hecho

--- main.py
+++ main.py
@@ -28,5 +28,40 @@ def menu1 ():
                 print("Opción invalida. Intentelo de nuevo.")
 
 
+##Segundo menu que le aparece al usuario.
+MENU_MOVIMIENTOS = """
+==============
+=== CAJERO ===
+==============
+1. --> Consultar saldo.
+2. --> Retirar saldo.
+3. --> Depositar saldo.
+4. --> Ver historial de movimientos
+5. --> Salir."""
+
+
+##Variable que controla cuantos movimientos desea hacer el usuario.
+cantidad_movimientos = int(input("Cuantos movimientos desea hacer. \n"))
+
+##Funcionalidad del MENU_MOVIMIENTOS.
+def menu2 ():
+    while True:
+        ##Variable que guarda que movimientos desea hacer el usuario
+        movimientos = input("Escoja una opción. \n")
+        match movimientos:
+            case "1": ##Consultar saldo
+                pass
+            case "2": ##Retirar saldo
+                pass
+            case "3": ##Depositar saldo
+                pass
+            case "4": ##Historial de movimientos
+                pass
+            case "5": ##Salir
+                break
+            case _: ##Caso de error
+                print("Opción invalida. Intentelo de nuevo.")
+
+
 ##Mensaje de despedida.
 print("Gracias por usar el cajero automático.")
